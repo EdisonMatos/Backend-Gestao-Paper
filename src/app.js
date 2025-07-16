2904a3431bb2663abb96ae9c3fbc5e24945658fa
Edison Matos <edison.matos@live.com>
2025-07-15 21:21:58 -0300

    feat: Atualiza cors

--- src/app.js
+++ src/app.js
@@ -2,14 +2,31 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://under-timer-front.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 const usuariosRouter = require("./modulos/usuarios");
 const clientesRouter = require("./modulos/clientes");
 const servicosRouter = require("./modulos/servicos");
 const comentariosRouter = require("./modulos/comentarios");
 
+app.use(express.json());
 app.use("/usuarios", usuariosRouter);
 app.use("/clientes", clientesRouter);
 app.use("/servicos", servicosRouter);
